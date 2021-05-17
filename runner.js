e16fa030466dfca612fde37159a15a2e351154aa
Francisco <javiermartincuenca@gmail.com>
2021-05-17 22:13:06 +0200

    Link suites with tests

--- runner.js
+++ runner.js
@@ -5,6 +5,8 @@ const emitter = new EventEmitter();
 
 const handlers = new Map();
 
+const stack = [];
+
 const describe = (title, handler) => {
   emitter.emit('add:suite', { title, handler });
 };
@@ -15,21 +17,36 @@ const it = (title, handler) => {
 
 emitter.on('add:suite', ({ title, handler }) => {
   const id = uuidv4();
+  const parent = stack.length ? stack[stack.length - 1] : undefined;
   handlers.set(id, {
     id,
     type: 'suite',
+    parent,
+    children: [],
     title,
   });
+
+  if (parent) {
+    handlers.get(parent).children.push(id);
+  }
+
+  stack.push(id);
   handler();
+  stack.pop();
 });
 
 emitter.on('add:test', ({ title, handler }) => {
   const id = uuidv4();
+  const parent = stack.length ? stack[stack.length - 1] : undefined;
+  if (parent) {
+    handlers.get(parent).children.push(id);
+  }
   handlers.set(id, {
     id,
     title,
     type: 'test',
-    handler
+    parent,
+    handler,
   });
   handler();
 });
